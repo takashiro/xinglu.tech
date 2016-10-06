90edcca77458ebe36b06bb42cdd010dc39337cc0
Takashiro <takashiro@qq.com>
2016-10-06 20:19:55 +0800

    某些类型不支持default

--- module/device/install.sql
+++ module/device/install.sql
@@ -1,11 +1,11 @@
 
 CREATE TABLE `pre_device` (
     `id` mediumint(8) unsigned NOT NULL auto_increment,
-    `name` varchar(50) NOT NULL DEFAULT '',
-    `type` varchar(50) NOT NULL DEFAULT '',
-    `location` varchar(255) NOT NULL DEFAULT '',
+    `name` varchar(50) NOT NULL,
+    `type` varchar(50) NOT NULL,
+    `location` varchar(255) NOT NULL,
     `status` tinyint(4) unsigned NOT NULL DEFAULT '0',
-    `admin` varchar(50) NOT NULL DEFAULT '',
-    `kindly_reminder` text NOT NULL DEFAULT '',
+    `admin` varchar(50) NOT NULL,
+    `kindly_reminder` text NOT NULL,
     PRIMARY KEY (`id`)
 ) Engine=MyISAM  DEFAULT CHARSET=utf8;
