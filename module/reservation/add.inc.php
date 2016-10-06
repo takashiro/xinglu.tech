462127f42ea27c3856a68003f1b438c925d12edb
Takashiro <takashiro@qq.com>
2016-10-06 22:20:07 +0800

    预约须记录用户ID

--- module/reservation/add.inc.php
+++ module/reservation/add.inc.php
@@ -2,6 +2,10 @@
 
 if(!defined('S_ROOT')) exit('access denied');
 
+if(!$_G['user']->isLoggedIn()){
+    showmsg('please_login_before_reserving_a_device', 'index.php?mod=user:login');
+}
+
 $deviceid = intval($_GET['deviceid'] ?? 0);
 $device = new Device($deviceid);
 if(!$device->exists()){
@@ -17,9 +21,6 @@ if($_POST){
     $month = max(1, $month);
     $date = intval($_POST['date'] ?? $today_date);
     $date = max(1, $date);
-    if($month < $today_month || $date <= $today_date){
-        showmsg('please_input_a_date_in_the_future', 'back');
-    }
 
     $hour_start = intval($_POST['hour_start'] ?? 0);
     $hour_start = max(1, min($hour_start, 23));
@@ -28,6 +29,10 @@ if($_POST){
 
     $time_start = rmktime($hour_start, 0, 0, $month, $date, $year);
     $time_end = rmktime($hour_end, 0, 0, $month, $date, $year);
+
+    if($time_start <= TIMESTAMP){
+        showmsg('please_input_a_date_in_the_future', 'back');
+    }
 
     $r = $db->fetch_first("SELECT id,time_start,time_end
         FROM {$tpre}reservation
@@ -42,6 +47,7 @@ if($_POST){
     }
 
     $reservation = new Reservation;
+    $reservation->userid = $_G['user']->id;
     $reservation->deviceid = $deviceid;
     $reservation->time_start = $time_start;
     $reservation->time_end = $time_end;
